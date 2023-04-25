d12230a90570ea34c642cbb1b1640b13fbcdfc41
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-04-25 10:57:45 +0900

    Output no line break at the end of the file if there is no line break at the file end

---
+++ src/iterator.rs
@@ -0,0 +1,30 @@
+use std::io::{self, BufRead};
+
+#[derive(Debug)]
+pub(crate) struct LinesWithEol<B> {
+    buf: B,
+}
+
+impl<B: BufRead> Iterator for LinesWithEol<B> {
+    type Item = io::Result<String>;
+
+    fn next(&mut self) -> Option<io::Result<String>> {
+        let mut buf = String::new();
+        match self.buf.read_line(&mut buf) {
+            Ok(0) => None,
+            Ok(_n) => Some(Ok(buf)),
+            Err(e) => Some(Err(e)),
+        }
+    }
+}
+
+pub(crate) trait IteratorExt {
+    fn lines_with_eol(self) -> LinesWithEol<Self>
+    where
+        Self: Sized,
+    {
+        LinesWithEol { buf: self }
+    }
+}
+
+impl<B: BufRead> IteratorExt for B {}
